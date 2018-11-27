b7a174b03d773679857ce1ce8efdcf531c0d8204
ehcayen <yanyi.x.chen@ericsson.com>
2018-11-27 13:27:05 +0800

    test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = vec!["This", "is", "a", "sentence"];
+
+    x.insert(1, "line");
+    x.insert(2, "contains");
+    x.remove(3);
+    x.push("about Rust");
+    x.pop();
+    for i in 0..x.len() {print!("{} ", x[i]);}
 }
