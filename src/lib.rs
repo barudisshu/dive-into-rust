e2392ab23a698d5695c32216d3f495c40c5b8468
Galudisu <galudisu@gmail.com>
2019-03-28 23:43:39 +0800

    float value calculate

--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,36 @@
-fn dec2_fact_string(nb: u64) -> String {
-    let mut a: String = "".to_string();
-    let mut step = 0;
-    let mut div = nb;
-    'a: loop {
-        if div == 0 {
-            break 'a;
+enum BinaryTree {
+    Leaf(i32),
+    Node(Box<BinaryTree>, i32, Box<BinaryTree>)
+}
+
+fn tree_weight_v1(t: BinaryTree) -> i32 {
+    match t {
+        BinaryTree::Leaf(payload) => payload,
+        BinaryTree::Node(left, payload, right) => {
+            tree_weight_v1(*left) + payload + tree_weight_v1(*right)
         }
-        step += 1;
-        let lef = div % step;
-        div = div / step;
-        a.push_str(&(lef.to_string()));
     }
-    a.chars().rev().collect()
 }
 
-fn fact_string_2dec(s: String) -> u64 {
-    let len = s.len() as u64;
-    let mut sum = 0;
-    for (i, c) in s.chars().enumerate() {
-        let k: u64 = match c.to_string().parse::<u64>() {
-            Ok(v) => v,
-            Err(_) => 0,
-        };
-        sum = sum * (len - i as u64) + k;
-    }
-    sum
-}
+/// Returns tree that Looks like:
+///
+///      +----(4)---+
+///      |          |
+///   +-(2)-+      [5]
+///   |     |
+///  [1]   [3]
+///
+fn sample_tree() -> BinaryTree {
+    let l1 = Box::new(BinaryTree::Leaf(1));
+    let l3 = Box::new(BinaryTree::Leaf(3));
+    let n2 = Box::new(BinaryTree::Node(l1, 2, l3));
+    let l5 = Box::new(BinaryTree::Leaf(5));
 
-fn testing1(nb: u64, exp: &str) -> () {
-    assert_eq!(&dec2_fact_string(nb), exp)
-}
-
-fn testing2(s: &str, exp: u64) -> () {
-    assert_eq!(fact_string_2dec(s.to_string()), exp)
+    BinaryTree::Node(n2, 4, l5)
 }
 
 #[test]
-fn basics_dec2_fact_string() {
-    testing1(2982, "4041000");
-    testing1(463, "341010");
-}
-
-#[test]
-fn basics_fact_string_2dec() {
-    testing2("4041000", 2982);
-    testing2("341010", 463);
+fn tree_demo_1() {
+    let tree = sample_tree();
+    assert_eq!(tree_weight_v1(tree), (1 + 2 + 3) + 4 + 5);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-fn main() {
-    let mut x = vec!["This", "is", "a", "sentence"];
+use std::num::Wrapping;
 
-    x.insert(1, "line");
-    x.insert(2, "contains");
-    x.remove(3);
-    x.push("about Rust");
-    x.pop();
-    for i in 0..x.len() {print!("{} ", x[i]);}
+fn main() {
+    // 变量small初始化为一个非常小的浮点数
+    let mut small = std::f32::EPSILON;
+    // 不断循环，让small 越来越近于0，直到最后等于0的状态
+    while small > 0.0 {
+        small = small / 2.0;
+        println!("{} {:?}", small, small.classify());
+    }
+
 }
